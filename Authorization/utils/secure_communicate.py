92e24f8ad53482bbd28877753ac504b7dff9590e
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 22:53:17 +0330

    chore: decode function base on key and message asci code relevant character of b64 decoded string.

--- Authorization/utils/secure_communicate.py
+++ Authorization/utils/secure_communicate.py
@@ -21,3 +21,11 @@ class Encryption:
                 enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
         return base64.urlsafe_b64encode("".join(enc).encode(errors="ignore")).decode(errors="ignore") 
     
+    @staticmethod
+    def Decode(key, message): 
+        dec = []              
+        message = base64.urlsafe_b64decode(message).decode(errors="ignore")
+        for i in range(len(message)):     
+                key_c = key[i % len(key)] 
+                dec.append(chr((256 + ord(message[i]) - ord(key_c)) % 256)) 
+        return "".join(dec)
