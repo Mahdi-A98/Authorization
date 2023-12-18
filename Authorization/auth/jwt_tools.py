3957371871efa3a7b57b181a7d67958e5ede2920
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 22:26:04 +0330

    chore: verify jti method to check existance of jti in database.

--- Authorization/auth/jwt_tools.py
+++ Authorization/auth/jwt_tools.py
@@ -9,3 +9,12 @@ def decode_jwt_token(jwt_token):
         print(e)
         return None, HTTPException(detail='Parse Error', status_code=status.HTTP_401_UNAUTHORIZED)
     return payload , None # Return payload and error if accures 
+
+def verify_jti(payload:dict):
+    jti = payload.get('jti')
+    if not databases['redis_db'].get(jti):
+        raise HTTPException(detail='Token not found', status_code=status.HTTP_401_UNAUTHORIZED)
+    if user_identifier:= databases['redis_db'].get(jti):
+        user_identifier = user_identifier.decode() if isinstance(user_identifier, bytes) else user_identifier
+        return user_identifier
+    return None
