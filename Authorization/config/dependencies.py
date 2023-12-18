24523eff0995d24ce0783eb46f1a012b8a59d44a
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 22:22:39 +0330

    chore: dependencies

--- Authorization/config/dependencies.py
+++ Authorization/config/dependencies.py
@@ -1,3 +1,16 @@
+# In the name of GOD
+from fastapi import Header, HTTPException, Request
+from typing import Annotated
+
+from services import SERVICE_CLASSES
+from auth.authentication import JWTAuthentication
+
+
+async def get_service(internal_service: Annotated[str, Header()]):
+    if not SERVICE_CLASSES.get(internal_service):
+        raise HTTPException(detail="Access denied.", status_code=403)
+    return SERVICE_CLASSES.get(internal_service)
+
 async def check_login_status(request: Request):
     access_token = request.headers.get("access-token")
     refresh_token = request.headers.get("refresh-token")
